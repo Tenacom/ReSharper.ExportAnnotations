4ba9f13eb543f073cfc588ef0beb50f9172eca38
Riccardo De Agostini <riccardo@tenacom.it>
2020-09-15 07:47:58 +0200

    Use the NoTargets SDK to build ReSharper.ExportAnnotations.Task.

--- src/ReSharper.ExportAnnotations.Task/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// We need a .cs file to avoid a compiler warning,
-// but the compiled assembly will not be included in package.
-namespace ReSharper.ExportAnnotations.Task
-{
-    class Program
-    {
-        public static void Main() { }
-    }
-}

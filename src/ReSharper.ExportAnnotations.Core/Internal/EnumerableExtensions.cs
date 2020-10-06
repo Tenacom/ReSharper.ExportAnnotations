4941135a99bb3260ff162b22091cecb9de043f49
Riccardo De Agostini <me@riccar.do>
2020-10-06 18:42:14 +0200

    Add net46 TFM to Core library.

--- src/ReSharper.ExportAnnotations.Core/Internal/EnumerableExtensions.cs
+++ src/ReSharper.ExportAnnotations.Core/Internal/EnumerableExtensions.cs
@@ -21,5 +21,19 @@ namespace ReSharper.ExportAnnotations.Internal
         public static IEnumerable<T> PrependIfNotNull<T>(this IEnumerable<T> @this, T? item)
             where T : class
             => item == null ? @this : @this.Prepend(item);
+
+#if NETFRAMEWORK
+        // Emulate System.Linq.Enumerable.Prepend, introduced in .NET Framework 4.7.1 / .NET Standard 1.6
+        // https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.prepend
+        private static IEnumerable<T> Prepend<T>(this IEnumerable<T> @this, T? item)
+            where T : class
+        {
+            yield return item!;
+            foreach (var originalItem in @this)
+            {
+                yield return originalItem;
+            }
+        }
+#endif
     }
 }
